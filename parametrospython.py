5ac556a659eb6700afe2d4e6a69bd49ce119fc1a
aderodriguezg-arch <ade.rodriguezg@gmail.com>
2026-02-15 23:38:24 -0600

    Update parametrospython.py

--- parametrospython.py
+++ parametrospython.py
@@ -1,1 +1,12 @@
+import sys
+def main():
+  if len(sys.argv) < 3:
+    print("Uso: python bienvenidos.py <param1> <param2>")
+    sys.exit(1)
 
+  nombre=sys.argv[1]
+  apellido=sys.argv[2]
+  print ('Su nombre es: ' + nombre + ' ' + apellido)
+
+if __name__ == "__main__":
+   main()
